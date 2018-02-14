1ddd2054a00c1512946e051040f6077412c11132
Loic Chesneau <loic.chesneau@lrqdo.fr>
2018-02-14 16:54:54 +0100

    ok tamer

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,11 @@ class Review < ApplicationRecord
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
   belongs_to :restaurant
+
+  def new
+    @restaurant = Restaurant.find_by_id(params[:id])
+  end
+
+  def create
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "seeding restaurants"
+
+Restaurant.create(name: "ABC", address: "6 rue des Goncourt", phone_number: "0667613337", category: "japanese")
+Restaurant.create(name: "DEF", address: "7 rue des Goncourt", phone_number: "0667613338", category: "chinese")
+Restaurant.create(name: "JKL", address: "8 rue des Goncourt", phone_number: "0667613339", category: "french")
+Restaurant.create(name: "MNO", address: "9 rue des Goncourt", phone_number: "0667613340", category: "french")
+Restaurant.create(name: "PQR", address: "10 rue des Goncourt", phone_number: "0667613341", category: "french")
+
+p "finished seeding"
